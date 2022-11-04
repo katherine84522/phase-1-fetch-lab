af8016e032dc5093902fe0602c1574ec27d0046b
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-11-04 00:38:01 -0400

    commit tree fba00c36121d3ea3559953ceede11cccd4e7d60f

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  window.fetch("https://anapioficeandfire.com/api/books/")
+    .then((resp) => resp.json())
+    .then((data) => {
+      renderBooks(data);
+    })
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
+
+
+
+
+
+
